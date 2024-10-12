2830d54856cb301db1617d8b52fb0fc1864c3ea7
tomasgz7 <tomasguzmancabj@hotmail.com>
2024-10-12 11:52:37 -0300

    form

--- app.js
+++ app.js
@@ -1,29 +1,58 @@
+// Seleccionar el formulario
 const inform = document.getElementById("inform");
 
+// Escuchar el evento de envío del formulario
 inform.addEventListener("submit", function (event) {
-  event.preventDefault();
+  event.preventDefault(); // Prevenir el comportamiento por defecto del formulario
 
-  const name = document.getElementById("name").value;
+  // Capturar los valores de los campos del formulario
+  const name = document.getElementById("nombre").value;
   const email = document.getElementById("email").value;
-  const message = document.getElementById("message").value;
+  const phone = document.getElementById("telefono").value;
+  const message = document.getElementById("mensaje").value;
 
+  // Crear el objeto con los datos del formulario
   const formData = {
-    to: email,
-    subject: `Nuevo mensaje de ${name}`,
-    text: message,
+    name: name,
+    email: email,
+    phone: phone,
+    message: message,
   };
 
+  // Enviar los datos al backend utilizando fetch
   fetch("http://localhost:3000/send-email", {
+    mode: "no-cors",
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
     body: JSON.stringify(formData),
   })
-    .then((response) => response.json())
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error en la solicitud");
+      }
+      return response.json();
+    })
     .then((data) => {
       console.log("Éxito:", data);
-      alert("Correo enviado con éxito");
+
+      // Mostrar el mensaje de éxito
+      const successMessage = document.getElementById("success-message");
+      successMessage.classList.remove("hidden");
+      successMessage.classList.add("opacity-100");
+
+      // Limpiar el formulario
+      inform.reset();
+
+      // Desaparecer el mensaje después de 3 segundos
+      setTimeout(function () {
+        successMessage.classList.add("opacity-0");
+        setTimeout(function () {
+          successMessage.classList.add("hidden");
+          successMessage.classList.remove("opacity-0", "opacity-100");
+        }, 1000); // 1 segundo de espera antes de esconder el mensaje
+      }, 3000); // El mensaje estará visible durante 3 segundos
     })
     .catch((error) => {
       console.error("Error:", error);
